fdca662495bab0600feb2a774c51dd6fe7fd0204
atfb84 <atfb84@mst.edu>
2023-04-28 14:22:46 -0700

    core data concepts

---
+++ quizzes/CoreDataConcepts/quartiles.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+data = [10, 12, 15, 18, 20, 21, 22, 24, 25, 27, 28, 30, 32, 35, 38, 40, 42, 45, 50]
+
+q1 = np.quantile(a=data, q=.25)
+q3 = np.quantile(a=data, q=.75)
+iqr = q3 - q1
+print(f'First Quartile: {q1}')
+print(f'Third Quartile: {q3}')
+print(f'IQR: {iqr}')
+print(np.quantile(a=data, q=.75))
+print(len(data))
